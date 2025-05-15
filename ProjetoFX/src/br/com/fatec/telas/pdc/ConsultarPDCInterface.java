ae3ea5cb7e83643b4593b4e901179b354338b1c0
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-05-15 20:36:14 -0300

    interfaces controllers

---
+++ ProjetoFX/src/br/com/fatec/telas/pdc/ConsultarPDCInterface.java
@@ -0,0 +1,43 @@
+package br.com.fatec.telas.pdc;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import recicla.model.bean.PontoColeta;
+import utils.NavigationUtil;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ArthurSelingin
+ */
+public class ConsultarPDCInterface {
+
+    @FXML
+    private TextField idField;
+    @FXML
+    private TextField novoNomeField;
+    @FXML
+    private TextArea resultadoArea;
+
+    @FXML
+    private void handleConsultar() {
+        try {
+            int id = Integer.parseInt(idField.getText());
+            String novoNome = novoNomeField.getText();
+
+            PontoColeta pc = new PontoColeta(id, "SimuladoLog", novoNome, "SimuladoQRCode");
+            resultadoArea.setText("Ponto alterado (simulado):\nID: " + pc.getId() + "\nNovo nome: " + pc.getNome());
+        } catch (Exception e) {
+            resultadoArea.setText("Erro: " + e.getMessage());
+        }
+    }
+        @FXML
+    private void onVoltarClick(javafx.event.ActionEvent event) {
+        NavigationUtil.voltarParaTelaInicial(event);
+    }
+}
