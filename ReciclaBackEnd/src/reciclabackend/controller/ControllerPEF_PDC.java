244576910ac1a9b1bd612020293bfcebd17bb627
Matheus Edivaldo <ms1501085@gmail.com>
2025-05-29 17:10:05 -0300

    Refatorado ControllerPEF_PDC para seguir padrão de instanciar DAO dentro dos métodos

--- ReciclaBackEnd/src/reciclabackend/controller/ControllerPEF_PDC.java
+++ ReciclaBackEnd/src/reciclabackend/controller/ControllerPEF_PDC.java
@@ -7,34 +7,33 @@ import reciclabackend.util.ControllerBasico;
 
 public class ControllerPEF_PDC implements ControllerBasico {
 
-    private final DaoPEF_PDC dao;
-
-    public ControllerPEF_PDC() throws SQLException, ClassNotFoundException {
-        this.dao = new DaoPEF_PDC();
-    }
-
     @Override
-    public Object inserir(Object obj) throws SQLException {
+    public Object inserir(Object obj) throws SQLException, ClassNotFoundException {
+        DaoPEF_PDC dao = new DaoPEF_PDC();
         return dao.inserir(obj);
     }
 
     @Override
-    public Object alterar(Object obj) throws SQLException {
+    public Object alterar(Object obj) throws SQLException, ClassNotFoundException {
+        DaoPEF_PDC dao = new DaoPEF_PDC();
         return dao.alterar(obj);
     }
 
     @Override
-    public boolean excluir(int id) throws SQLException {
+    public boolean excluir(int id) throws SQLException, ClassNotFoundException {
+        DaoPEF_PDC dao = new DaoPEF_PDC();
         return dao.excluir(id);
     }
 
     @Override
-    public Object buscar(int id) throws SQLException {
+    public Object buscar(int id) throws SQLException, ClassNotFoundException {
+        DaoPEF_PDC dao = new DaoPEF_PDC();
         return dao.buscar(id);
     }
 
     @Override
-    public List<Object> listar(String filtro) throws SQLException {
+    public List<Object> listar(String filtro) throws SQLException, ClassNotFoundException {
+        DaoPEF_PDC dao = new DaoPEF_PDC();
         return dao.listar(filtro);
     }
 }
