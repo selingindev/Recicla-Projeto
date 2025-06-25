1f346f865bfcd5b937909c1ae5d9b7b80da7cf62
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-06-24 21:34:44 -0300

    Refatoração dos métodos de exclusão, busca e listagem na classe ControllerPessoaFisica e DaoPessoaFisica para melhorar a legibilidade e a lógica de filtragem.

--- ReciclaBackEnd/src/reciclabackend/controller/ControllerPessoaFisica.java
+++ ReciclaBackEnd/src/reciclabackend/controller/ControllerPessoaFisica.java
@@ -34,19 +34,19 @@ public class ControllerPessoaFisica implements ControllerBasico {
     }
 
     @Override
-    public boolean excluir(Object obj) throws SQLException, ClassNotFoundException {
+    public boolean excluir(int id) throws SQLException, ClassNotFoundException {
         dao = new DaoPessoaFisica();
         return dao.excluir(id);
     }
 
     @Override
-    public Object buscar(Object obj) throws SQLException, ClassNotFoundException {
+    public Object buscar(int id) throws SQLException, ClassNotFoundException {
         dao = new DaoPessoaFisica();
         return dao.buscar(id);
     }
 
     @Override
-    public List<Object> listar(Object obj) throws SQLException, ClassNotFoundException {
+    public List<Object> listar(String pfiltro) throws SQLException, ClassNotFoundException {
         dao = new DaoPessoaFisica();
         return dao.listar(pfiltro);
     }
