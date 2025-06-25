c33ff641c88575346170dc63c4f29a125bf782b1
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-06-24 22:11:47 -0300

    arrumando manter

--- ReciclaBackEnd/src/reciclabackend/view/ManterParametro.java
+++ ReciclaBackEnd/src/reciclabackend/view/ManterParametro.java
@@ -1,104 +1,93 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package reciclabackend.view;
 
 import java.sql.SQLException;
 import java.util.List;
-
 import javax.swing.JOptionPane;
-
 import reciclabackend.controller.ControllerParametro;
 import reciclabackend.model.bean.Parametro;
-
 import reciclabackend.util.ViewBasico;
 
 public class ManterParametro implements ViewBasico {
+
     @Override
-    public  void menu() throws SQLException, ClassNotFoundException {
-     String msg = " 1 - Inserir \n 2 - Alterar \n 3 - buscar \n 4 - excluir \n 5 - Listar";
-        int num = Integer.parseInt(JOptionPane.showInputDialog(msg));
-        switch (num) {
-            case 1:
-                inserir();
-                break;
-            case 2 :
-                alterar();
-                break;
-            case 3: 
-                buscar();
-                break;
-            case 4 :
-                excluir();
-                break;
-            case 5 :
-                listar();
-                break; 
-            default :
-                System.out.println("Opcao inválida");
-                break;
+    public void menu() throws SQLException, ClassNotFoundException {
+        String msg = "1 - Inserir\n2 - Alterar\n3 - Buscar\n4 - Excluir\n5 - Listar\n0 - Sair";
+        while (true) {
+            int opc = Integer.parseInt(JOptionPane.showInputDialog(msg));
+            if (opc == 0) return;
+            switch (opc) {
+                case 1 -> inserir();
+                case 2 -> alterar();
+                case 3 -> buscar();
+                case 4 -> excluir();
+                case 5 -> listar();
+                default -> JOptionPane.showMessageDialog(null, "Opção inválida");
+            }
         }
     }
 
     @Override
-    public  void inserir() throws SQLException, ClassNotFoundException {
-        int cod = Integer.parseInt(JOptionPane.showInputDialog(null, "CÓDIGO")); 
-        String descricao = JOptionPane.showInputDialog(null, "DESCRICÃO");
-        
+    public void inserir() throws SQLException, ClassNotFoundException {
+        int cod = Integer.parseInt(JOptionPane.showInputDialog("CÓDIGO"));
+        String descricao = JOptionPane.showInputDialog("DESCRIÇÃO");
+
         Parametro parEntrada = new Parametro(cod, descricao);
-        ControllerParametro controllerParametro = new ControllerParametro();
-        Parametro parSaida = (Parametro) controllerParametro.inserir(parEntrada);
+        ControllerParametro controller = new ControllerParametro();
+        Parametro parSaida = (Parametro) controller.inserir(parEntrada);
 
         JOptionPane.showMessageDialog(null, parSaida.toString());
-            
     }
 
     @Override
     public void alterar() throws SQLException, ClassNotFoundException {
-        int cod = Integer.parseInt(JOptionPane.showInputDialog(null, "CÓDIGO"));
-        String descricao = JOptionPane.showInputDialog(null, "DESCRICAO"); 
-        
+        int cod = Integer.parseInt(JOptionPane.showInputDialog("CÓDIGO"));
+        String descricao = JOptionPane.showInputDialog("DESCRIÇÃO");
+
         Parametro parEntrada = new Parametro(cod, descricao);
-        ControllerParametro controllerParametro = new ControllerParametro();
-        Parametro parSaida = (Parametro) controllerParametro.alterar(parEntrada);
+        ControllerParametro controller = new ControllerParametro();
+        Parametro parSaida = (Parametro) controller.alterar(parEntrada);
 
         JOptionPane.showMessageDialog(null, parSaida.toString());
     }
-    
+
     @Override
     public void excluir() throws SQLException, ClassNotFoundException {
-        int cod = Integer.parseInt(JOptionPane.showInputDialog(null, "CÓDIGO"));
+        int cod = Integer.parseInt(JOptionPane.showInputDialog("CÓDIGO"));
 
-        ControllerParametro controllerParametro = new ControllerParametro();
-        Boolean parSaida = controllerParametro.excluir(cod);
+        ControllerParametro controller = new ControllerParametro();
+        boolean sucesso = controller.excluir(cod);
 
-        JOptionPane.showMessageDialog(null, parSaida.toString());
-        
+        JOptionPane.showMessageDialog(null, sucesso ? "Excluído com sucesso!" : "Erro ao excluir.");
     }
 
     @Override
     public void buscar() throws SQLException, ClassNotFoundException {
-        int cod = Integer.parseInt(JOptionPane.showInputDialog(null, "CÓDIGO"));
-        String descricao = JOptionPane.showInputDialog(null, "DESCRICAO");
-        
-        ControllerParametro controllerParametro = new ControllerParametro();
-        String dadosPar = (String) controllerParametro.buscar(cod);
-        JOptionPane.showMessageDialog(null, dadosPar);
-        
+        int cod = Integer.parseInt(JOptionPane.showInputDialog("CÓDIGO"));
+
+        ControllerParametro controller = new ControllerParametro();
+        Parametro parSaida = (Parametro) controller.buscar(cod);
+
+        JOptionPane.showMessageDialog(null,
+                parSaida != null ? parSaida.toString() : "Parâmetro não encontrado.");
     }
 
     @Override
     public void listar() throws SQLException, ClassNotFoundException {
-        int cod = Integer.parseInt(JOptionPane.showInputDialog(null, "CÓDIGO"));
-        String descricao = JOptionPane.showInputDialog(null, "DESCRICAO");
-        
-        ControllerParametro controllerParametro = new ControllerParametro();
-        List<Object> listaParametro = controllerParametro.listar(descricao);
-        for(Object ParametroItem : listaParametro){
-            Parametro parametro = (Parametro) ParametroItem;
-            JOptionPane.showMessageDialog(null, parametro.toString());
+        String filtro = JOptionPane.showInputDialog("Digite parte da descrição para filtrar (deixe vazio para todos):");
+        if (filtro == null) filtro = "";
+
+        ControllerParametro controller = new ControllerParametro();
+        List<Object> listaParametro = controller.listar(filtro.trim());
+
+        if (listaParametro.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Nenhum parâmetro encontrado.");
+        } else {
+            StringBuilder sb = new StringBuilder();
+            for (Object obj : listaParametro) {
+                Parametro parametro = (Parametro) obj;
+                sb.append(parametro.toString()).append("\n");
+            }
+            JOptionPane.showMessageDialog(null, sb.toString());
         }
     }
-    
 }
--- ReciclaBackEnd/src/reciclabackend/view/ManterPontoReciclagem.java
+++ ReciclaBackEnd/src/reciclabackend/view/ManterPontoReciclagem.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package reciclabackend.view;
 
 import java.sql.SQLException;
@@ -11,100 +7,87 @@ import reciclabackend.controller.ControllerPontoReciclagem;
 import reciclabackend.model.bean.PontoReciclagem;
 import reciclabackend.util.ViewBasico;
 
-/**
- *
- * @author LAB 211
- */
 public class ManterPontoReciclagem implements ViewBasico {
 
     @Override
     public void menu() throws SQLException, ClassNotFoundException {
-        String msg = " 1 - Inserir \n 2 - Alterar \n 3 - buscar \n 4 - excluir \n 5 - Listar " ;
-        int num = Integer.parseInt(JOptionPane.showInputDialog(msg));
-        switch (num) {
-            case 1 : 
-                inserir();
-                break;
-            case 2 : 
-                alterar();
-                break;
-            case 3 : 
-                buscar();
-                break;
-            case 4 : 
-                excluir();
-                break;
-            case 5 : 
-                listar();
-                break;
-            default : 
-                System.out.println("Opcao inválida");
-                break;
+        String msg = "1 - Inserir\n2 - Alterar\n3 - Buscar\n4 - Excluir\n5 - Listar";
+        int opcao = Integer.parseInt(JOptionPane.showInputDialog(msg));
+        
+        switch (opcao) {
+            case 1: inserir(); break;
+            case 2: alterar(); break;
+            case 3: buscar(); break;
+            case 4: excluir(); break;
+            case 5: listar(); break;
+            default: JOptionPane.showMessageDialog(null, "Opção inválida."); break;
         }
     }
 
     @Override
     public void inserir() throws SQLException, ClassNotFoundException {
-        int idPEJ = Integer.parseInt(JOptionPane.showInputDialog("idPEJ"));
-        String nome = JOptionPane.showInputDialog("NOME");
-        PontoReciclagem idEnt = new PontoReciclagem(idPEJ,nome);
-        ControllerPontoReciclagem contId = new ControllerPontoReciclagem();
-        PontoReciclagem idSaida = (PontoReciclagem) contId.inserir(idEnt);
-        JOptionPane.showMessageDialog(null,idSaida.toString());
+        int idPEJ = Integer.parseInt(JOptionPane.showInputDialog("ID da Pessoa Jurídica:"));
+        String nome = JOptionPane.showInputDialog("Nome do Ponto de Reciclagem:");
+        
+        PontoReciclagem ponto = new PontoReciclagem(idPEJ, nome);
+        ControllerPontoReciclagem controller = new ControllerPontoReciclagem();
+        PontoReciclagem result = (PontoReciclagem) controller.inserir(ponto);
+        
+        JOptionPane.showMessageDialog(null, result.toString());
     }
 
     @Override
     public void alterar() throws SQLException, ClassNotFoundException {
-        int id = Integer.parseInt(JOptionPane.showInputDialog("ID"));
-        int idPEJ = Integer.parseInt(JOptionPane.showInputDialog("idPEJ"));
-        String nome = JOptionPane.showInputDialog("NOME");
-        PontoReciclagem idEnt = new PontoReciclagem(id,idPEJ,nome);
-        ControllerPontoReciclagem contId = new ControllerPontoReciclagem();
-        PontoReciclagem idSaida = (PontoReciclagem) contId.alterar(idEnt);
-        JOptionPane.showMessageDialog(null,idSaida.toString());
+        int id = Integer.parseInt(JOptionPane.showInputDialog("ID:"));
+        int idPEJ = Integer.parseInt(JOptionPane.showInputDialog("ID da Pessoa Jurídica:"));
+        String nome = JOptionPane.showInputDialog("Novo nome do Ponto de Reciclagem:");
+        
+        PontoReciclagem ponto = new PontoReciclagem(id, idPEJ, nome);
+        ControllerPontoReciclagem controller = new ControllerPontoReciclagem();
+        PontoReciclagem result = (PontoReciclagem) controller.alterar(ponto);
+        
+        JOptionPane.showMessageDialog(null, result.toString());
     }
 
     @Override
     public void buscar() throws SQLException, ClassNotFoundException {
-        int id = Integer.parseInt(JOptionPane.showInputDialog("ID"));
-        PontoReciclagem idEnt = new PontoReciclagem(id);
-        ControllerPontoReciclagem contId = new ControllerPontoReciclagem();
-        PontoReciclagem idSaida = (PontoReciclagem) contId.buscar(idEnt);
-        JOptionPane.showMessageDialog(null,idSaida.toString());
+        int id = Integer.parseInt(JOptionPane.showInputDialog("ID:"));
+        
+        ControllerPontoReciclagem controller = new ControllerPontoReciclagem();
+        PontoReciclagem result = (PontoReciclagem) controller.buscar(id);
+        
+        JOptionPane.showMessageDialog(null, result.toString());
     }
 
     @Override
     public void excluir() throws SQLException, ClassNotFoundException {
-        int id = Integer.parseInt(JOptionPane.showInputDialog("ID"));
-        PontoReciclagem idEnt = new PontoReciclagem(id);
-        ControllerPontoReciclagem contId = new ControllerPontoReciclagem();
-        PontoReciclagem idSaida = (PontoReciclagem) contId.excluir(idEnt);
-        JOptionPane.showMessageDialog(null,idSaida.toString());
+        int id = Integer.parseInt(JOptionPane.showInputDialog("ID:"));
+        
+        ControllerPontoReciclagem controller = new ControllerPontoReciclagem();
+        boolean result =  controller.excluir(id);
+        
+        JOptionPane.showMessageDialog(null, "Excluído: " + result);
     }
 
     @Override
     public void listar() throws SQLException, ClassNotFoundException {
-        int idPEJ = Integer.parseInt(JOptionPane.showInputDialog("idPEJ"));
-        PontoReciclagem idEnt = new PontoReciclagem(idPEJ);
-
-
-        ControllerPontoReciclagem contId = new ControllerPontoReciclagem();
-        List<Object> listaUsuario = contId.listar(idEnt);
-        System.out.println(listaUsuario);
-        for (Object usuObj : listaUsuario) {
-            System.out.println(usuObj.toString());
-            PontoReciclagem usuSaida = (PontoReciclagem) usuObj;
-            JOptionPane.showMessageDialog(null,usuSaida.toString());
+        String filtro = JOptionPane.showInputDialog("Filtrar por ID da Pessoa Jurídica (deixe em branco para todos):");
+        ControllerPontoReciclagem controller = new ControllerPontoReciclagem();
+        List<Object> lista = controller.listar(filtro);
+        
+        for (Object obj : lista) {
+            PontoReciclagem ponto = (PontoReciclagem) obj;
+            JOptionPane.showMessageDialog(null, ponto.toString());
         }
     }
 
     public static boolean validar() throws SQLException, ClassNotFoundException {
-        int idPEJ = Integer.parseInt(JOptionPane.showInputDialog("idPEJ"));
-        String nome = JOptionPane.showInputDialog("NOME");
-        PontoReciclagem idEnt = new PontoReciclagem(idPEJ,nome);
-        ControllerPontoReciclagem contId = new ControllerPontoReciclagem();
-        return contId.validar(idEnt);
+        int idPEJ = Integer.parseInt(JOptionPane.showInputDialog("ID da Pessoa Jurídica:"));
+        String nome = JOptionPane.showInputDialog("Nome:");
+        
+        PontoReciclagem ponto = new PontoReciclagem(idPEJ, nome);
+        ControllerPontoReciclagem controller = new ControllerPontoReciclagem();
+        
+        return controller.validar(ponto);
     }
-
-    
 }
