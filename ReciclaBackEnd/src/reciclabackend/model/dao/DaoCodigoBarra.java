7a7dde3403f088f262dfcb286bd10c0951764757
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2025-06-26 12:08:51 -0300

    corrigindo dao

--- ReciclaBackEnd/src/reciclabackend/model/dao/DaoCodigoBarra.java
+++ ReciclaBackEnd/src/reciclabackend/model/dao/DaoCodigoBarra.java
@@ -5,10 +5,10 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import reciclabackend.model.bean.CodigoBarras;
-import reciclabackend.model.bean.Colaborador;
 import reciclabackend.util.DaoBasico;
 
 public class DaoCodigoBarra implements DaoBasico {
@@ -19,57 +19,101 @@ public class DaoCodigoBarra implements DaoBasico {
         this.c = c;
     }
 
+    public DaoCodigoBarra() {
+        //TODO Auto-generated constructor stub
+    }
+
     @Override
     public Object inserir(Object obj) throws SQLException {
-	CodigoBarras cbEnt = (CodigoBarras) obj;
-		
-		String sql = "INSERT INTO CODIGOBARRAS" + 
-					 "(description, gtdin, brandName)" + 
-				 	" values (?, ?, ?)";
-		
-		try {
-			PreparedStatement stmt;
-			
-			stmt = c.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-		
-		
-			stmt.setString(1, cbEnt.getDescription());
-			stmt.setString(1, cbEnt.getGtdin());
-			stmt.setString(1, cbEnt.getBrandName());
-			
-			stmt.executeUpdate();
-			
-			stmt.close();
-			return cbEnt;
-		} catch (SQLException e) {
-			e.printStackTrace();
-			return null;
-		}
-		
-	}
+        CodigoBarras cbEnt = (CodigoBarras) obj;
+
+        String sql = "INSERT INTO CODIGOBARRAS (description, gtdin, brandName) VALUES (?, ?, ?)";
+
+        try (PreparedStatement stmt = c.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            stmt.setString(1, cbEnt.getDescription());
+            stmt.setString(2, cbEnt.getGtdin());
+            stmt.setString(3, cbEnt.getBrandName());
+
+            stmt.executeUpdate();
+
+            ResultSet rs = stmt.getGeneratedKeys();
+            if (rs.next()) {
+                cbEnt.setId(rs.getInt(1));
+            }
+
+            return cbEnt;
+        }
+    }
 
     @Override
     public Object alterar(Object obj) throws SQLException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'alterar'");
+        CodigoBarras cbEnt = (CodigoBarras) obj;
+
+        String sql = "UPDATE CODIGOBARRAS SET description = ?, gtdin = ?, brandName = ? WHERE id = ?";
+
+        try (PreparedStatement stmt = c.prepareStatement(sql)) {
+            stmt.setString(1, cbEnt.getDescription());
+            stmt.setString(2, cbEnt.getGtdin());
+            stmt.setString(3, cbEnt.getBrandName());
+            stmt.setInt(4, cbEnt.getId());
+
+            stmt.executeUpdate();
+            return cbEnt;
+        }
     }
 
     @Override
     public boolean excluir(int id) throws SQLException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'excluir'");
+        String sql = "DELETE FROM CODIGOBARRAS WHERE id = ?";
+
+        try (PreparedStatement stmt = c.prepareStatement(sql)) {
+            stmt.setInt(1, id);
+            int rows = stmt.executeUpdate();
+            return rows > 0;
+        }
     }
 
     @Override
     public Object buscar(int id) throws SQLException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'buscar'");
+        String sql = "SELECT * FROM CODIGOBARRAS WHERE id = ?";
+
+        try (PreparedStatement stmt = c.prepareStatement(sql)) {
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                CodigoBarras cb = new CodigoBarras(
+                        rs.getInt("id"),
+                        rs.getString("description"),
+                        rs.getString("gtdin"),
+                        rs.getString("brandName"));
+                return cb;
+            }
+
+            return null; // Retorna null se não encontrar
+        }
     }
 
     @Override
     public List<Object> listar(String pfiltro) throws SQLException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'listar'");
+        String sql = "SELECT * FROM CODIGOBARRAS WHERE gtdin LIKE ?";
+        List<Object> lista = new ArrayList<>();
+
+        try (PreparedStatement stmt = c.prepareStatement(sql)) {
+            stmt.setString(1, "%" + pfiltro + "%");
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                CodigoBarras cb = new CodigoBarras(
+                        rs.getInt("id"),
+                        rs.getString("description"),
+                        rs.getString("gtdin"),
+                        rs.getString("brandName"));
+                lista.add(cb);
+            }
+
+            return lista;
+        }
     }
 
 }
